091afba45d6247af8e1fa39277fdfeab3e902c9f
zuheo23 <zuheo23@zakoc.com>
2016-10-03 17:01:16 +1100

    Removed Search button, Added TreeView content, Added "On Enter" event handler

--- src/ui/ExplorerController.java
+++ src/ui/ExplorerController.java
@@ -5,9 +5,22 @@
  */
 package ui;
 
+import java.io.IOException;
 import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.control.TreeView;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import ui.util.DirectoryCollector;
 
 /**
  * FXML Controller class
@@ -16,12 +29,61 @@ import javafx.fxml.Initializable;
  */
 public class ExplorerController implements Initializable {
 
+    @FXML
+    private TextField tfPath;
+    @FXML
+    private Label lblInfo;
+    @FXML
+    private TreeView<String> tvDirs;
+
+    private UI ui;
+
+    private Path cwd;
+
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
-    
+        cwd = Paths.get(".").toAbsolutePath().normalize();
+        lblInfo.setText(cwd.toString());
+        getAllDirs();
+    }
+
+    @FXML
+    public void chgDir() {
+        String newDir = tfPath.getText();
+        Path newRelPath = cwd.resolve(newDir).normalize();
+        Path newAbsPath = cwd.resolve(Paths.get(newDir).normalize());
+        if (Files.exists(newRelPath)) {
+            lblInfo.setText(String.format("%s: Changing to %s", cwd.toString(), newRelPath.toString()));
+            cwd = newRelPath.toAbsolutePath();
+        } else if (Files.exists(newAbsPath)) {
+            lblInfo.setText(String.format("%s: Changing to %s", cwd.toString(), newAbsPath.toString()));
+            cwd = newAbsPath;
+        } else {
+            lblInfo.setText(String.format("%s: Path is invalid", cwd.toString()));
+        }
+        getAllDirs();
+    }
+
+    @FXML
+    public void handleKeyPressed(KeyEvent event) {
+        if (event.getCode() == KeyCode.ENTER) {
+            chgDir();
+        }
+    }
+
+    private void getAllDirs() {
+        try {
+            DirectoryCollector.PrintDirectories pd = new DirectoryCollector.PrintDirectories();
+            System.out.println("CWD: " + cwd);
+            Files.walkFileTree(cwd, pd);
+            pd.root.setExpanded(true);
+            tvDirs.setRoot(pd.root);
+
+        } catch (IOException ex) {
+            Logger.getLogger(ExplorerController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
 }
